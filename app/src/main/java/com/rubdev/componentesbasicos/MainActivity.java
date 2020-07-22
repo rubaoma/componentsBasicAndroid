bfe322835fbc596079212f29fd3cdf35e78a9d0e
Rubens MA <rubao.ma@gmail.com>
2020-07-22 10:01:26 -0300

    radioButton options Sex Male and female

--- app/src/main/java/com/rubdev/componentesbasicos/MainActivity.java
+++ app/src/main/java/com/rubdev/componentesbasicos/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -16,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText campoEmail;
     private TextView textoResultado;
 
+    private CheckBox checkVerde, checkBranco, checkVermelho;
+
+    private RadioButton radioMasculino, radioFeminino;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +32,59 @@ public class MainActivity extends AppCompatActivity {
         campoEmail = findViewById(R.id.editEmail);
         textoResultado = findViewById(R.id.textResultado);
 
+        //checkBox
+        checkVerde = findViewById(R.id.checkVerde);
+        checkBranco = findViewById(R.id.checkBranco);
+        checkVermelho = findViewById(R.id.checkVermelho);
+
+        radioMasculino = findViewById(R.id.radioMasculino);
+        radioFeminino = findViewById(R.id.radioFeminino);
+
 
 
     }
 
-    public void enviar(View view){
+    public void radioButton(){
+        String texto = "";
+        if( radioMasculino.isChecked()){
+            String opcao = radioMasculino.getText().toString();
+            texto = opcao + " selecionado";
+        }
+        if( radioFeminino.isChecked()){
+            String opcao = radioFeminino.getText().toString();
+            texto = opcao + " selecionado";
+        }
+        textoResultado.setText(texto);
+    }
 
+
+    public void  checkbox() {
+
+        String texto = "";
+        if( checkVerde.isChecked()){
+            String corSelecionada = checkVerde.getText().toString();
+            texto = corSelecionada+" selecionado -";
+            //texto = "Verde selecionado - ";
+        }
+        if( checkBranco.isChecked()){
+            texto = texto + "Branco selecionado - ";
+        }
+        if( checkVermelho.isChecked()){
+            texto = texto + "Vermelho selecionado - ";
+        }
+
+        textoResultado.setText( texto );
+    }
+
+
+    public void enviar(View view){
+            radioButton();
+        //checkbox();
+        /*
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
         textoResultado.setText("nome: "+ nome + " email:"+ email);
-
+        */
     }
 
     public void limpar(View view){
